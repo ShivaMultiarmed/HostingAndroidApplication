d3385a9f2ec854bbba7b160913203ed28826d1d5
Mikhail_Shell <theoutstandingme@gmail.com>
2025-01-27 13:36:41 +0700

    successfully uploaded video larger than 50MB

--- app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/ContentResolving.kt
+++ app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/ContentResolving.kt
@@ -4,10 +4,22 @@ import android.content.ContentResolver
 import android.net.Uri
 import android.webkit.MimeTypeMap
 import mikhail.shell.video.hosting.domain.models.File
+import java.io.ByteArrayOutputStream
+import kotlin.math.min
 
 fun ContentResolver.getFileBytes(uri: Uri): ByteArray? {
-    val inputStream = this.openInputStream(uri)
-    return inputStream?.readBytes()
+    val inputStream = this.openInputStream(uri) ?: return null
+    return inputStream.use { input ->
+        val outputStream = ByteArrayOutputStream()
+        outputStream.use { output ->
+            val buffer = ByteArray(1024)
+            var bytesRead: Int
+            while (input.read(buffer).also { bytesRead = it } != -1) {
+                output.write(buffer, 0, bytesRead)
+            }
+            output.toByteArray()
+        }
+    }
 }
 
 fun ContentResolver.uriToFile(uri: Uri): File {
