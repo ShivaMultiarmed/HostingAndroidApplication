47d8e38abe9d6b9f381a5588945f7ac03a318642
Mikhail_Shell <theoutstandingme@gmail.com>
2025-01-30 14:19:44 +0700

    now video is paused on navigating away or moving the app to background

--- app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/Player.kt
+++ app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/Player.kt
@@ -1,9 +1,27 @@
 package mikhail.shell.video.hosting.presentation.utils
 
+import android.app.Activity
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.media.AudioManager.AUDIOFOCUS_GAIN
+import android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT
+import android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE
+import android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK
+import android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.Player
 import androidx.media3.ui.PlayerView
 
@@ -12,6 +30,18 @@ fun PlayerComponent(
     modifier: Modifier = Modifier,
     player: Player
 ) {
+    var savedPlayState by rememberSaveable { mutableStateOf(false) }
+    val pauseActions = {
+        if (!(savedPlayState && !player.isPlaying))
+            savedPlayState = player.isPlaying
+        player.pause()
+    }
+    val playActions = {
+        player.play()
+    }
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val windowDecorView = (context as Activity).window.decorView
     AndroidView(
         modifier = modifier,
         factory = {
@@ -24,4 +54,38 @@ fun PlayerComponent(
             }
         }
     )
+    DisposableEffect(lifecycleOwner) {
+        val audioManager = context.getSystemService(AudioManager::class.java)
+        val audioListener = AudioManager.OnAudioFocusChangeListener {
+            if (it == AUDIOFOCUS_LOSS_TRANSIENT) {
+                pauseActions()
+            } else if (it in arrayOf(AUDIOFOCUS_GAIN, AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK, AUDIOFOCUS_GAIN_TRANSIENT, AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE)) {
+                playActions()
+            }
+        }
+        val audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+            .setOnAudioFocusChangeListener(audioListener).build()
+        audioManager.requestAudioFocus(audioFocusRequest)
+        val eventObserver = LifecycleEventObserver { owner, event ->
+            if (event == Lifecycle.Event.ON_PAUSE && windowDecorView.hasWindowFocus()) {
+                pauseActions()
+            } else if (event == Lifecycle.Event.ON_RESUME && savedPlayState) {
+                playActions()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(eventObserver)
+        val windowFocusListener = ViewTreeObserver.OnWindowFocusChangeListener {
+            if (!it) {
+                pauseActions()
+            } else if (savedPlayState) {
+                playActions()
+            }
+        }
+        windowDecorView.viewTreeObserver.addOnWindowFocusChangeListener(windowFocusListener)
+        onDispose {
+            audioManager.abandonAudioFocusRequest(audioFocusRequest)
+            lifecycleOwner.lifecycle.removeObserver(eventObserver)
+            windowDecorView.viewTreeObserver.removeOnWindowFocusChangeListener(windowFocusListener)
+        }
+    }
 }
