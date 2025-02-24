eff97008c47df8c46f06b3af38d27d9fcb71ff74
Mikhail_Shell <theoutstandingme@gmail.com>
2025-02-24 15:45:24 +0700

    make miniplayer smaller

--- app/src/main/kotlin/mikhail/shell/video/hosting/presentation/video/MiniPlayer.kt
+++ app/src/main/kotlin/mikhail/shell/video/hosting/presentation/video/MiniPlayer.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
@@ -25,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
@@ -50,13 +50,12 @@ fun MiniPlayer(
             }
         }
     )
-    var pipWidth by remember { mutableStateOf(0.dp) }
+    var pipWidth by remember { mutableStateOf(200.dp) }
     if (isPrepared && shouldPlay) {
-        val density = LocalDensity.current
         PipContainer(
             modifier = Modifier
                 .wrapContentSize()
-                .fillMaxWidth(1/5f)
+                .width(pipWidth)
         ) {
             // TODO
             var topBarAlpha by rememberSaveable { mutableFloatStateOf(1f) }
