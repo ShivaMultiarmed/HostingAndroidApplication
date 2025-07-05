08a3f09b507c5d7eb9cf686ba7bda701a1f31eba
Mikhail_Shell <theoutstandingme@gmail.com>
2025-07-05 20:13:43 +0700

    Changed app version to 6 (1.2.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "mikhail.shell.video.hosting"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.2.2"
         testInstrumentationRunner = "mikhail.shell.video.hosting.HostingTestsRunner"
     }
