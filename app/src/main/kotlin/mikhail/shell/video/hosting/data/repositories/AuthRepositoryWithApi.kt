ac5d51f71bbcfc1efe0a4322c385e7a9b6119c22
Mikhail_Shell <theoutstandingme@gmail.com>
2025-07-23 22:58:37 +0700

    Made Result error type supertype Error

--- app/src/main/kotlin/mikhail/shell/video/hosting/data/repositories/AuthRepositoryWithApi.kt
+++ app/src/main/kotlin/mikhail/shell/video/hosting/data/repositories/AuthRepositoryWithApi.kt
@@ -1,7 +1,6 @@
 package mikhail.shell.video.hosting.data.repositories
 
 import android.content.Context
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -27,24 +26,37 @@ import javax.inject.Inject
 class AuthRepositoryWithApi @Inject constructor(
     @ApplicationContext private val appContext: Context,
     private val authApi: AuthApi,
-    private val fcm: FirebaseMessaging,
     private val gson: Gson
 ) : AuthRepository {
     override suspend fun signInWithPassword(
         email: String,
         password: String
-    ): Result<AuthModel, CompoundError<SignInError>> {
-        return try {
-            Result.Success(
-                authApi.signInWithPassword(email, password)
-            )
-        } catch (e: HttpException) {
-            val json = e.response()?.errorBody()?.string()
-            val type = object : TypeToken<CompoundError<SignInError>>() {}.type
-            val compoundError = gson.fromJson(json, type)?: CompoundError(SignInError.UNEXPECTED)
-            Result.Failure(compoundError)
-        } catch (e: Exception) {
-            Result.Failure(CompoundError(SignInError.UNEXPECTED))
+    ): Result<AuthModel, Error> {
+        return if (appContext.isNetworkAvailable()) {
+            try {
+                Result.Success(authApi.signInWithPassword(email, password))
+            } catch (e: HttpException) {
+                val error: Error = when (e.code()) {
+                    400 -> {
+                        val json = e.response()?.errorBody()?.string()
+                        val type = object : TypeToken<CompoundError<SignInError>>() {}.type
+                        gson.fromJson(json, type) ?: SignInError.UNEXPECTED
+                    }
+                    401, 403 -> NetworkError.FORBIDDEN
+                    404 -> NetworkError.NOT_FOUND
+                    in 500..599 -> NetworkError.SERVER_ERROR
+                    else -> NetworkError.UNEXPECTED
+                }
+                Result.Failure(error)
+            } catch (_: SocketTimeoutException) {
+                Result.Failure(NetworkError.TIMEOUT_EXCEEDED)
+            } catch (_: IOException) {
+                Result.Failure(NetworkError.SERVER_NOT_AVAILABLE)
+            } catch (_: Exception) {
+                Result.Failure(SignInError.UNEXPECTED)
+            }
+        } else {
+            Result.Failure(NetworkError.CONNECTION_ERROR)
         }
     }
 
@@ -53,7 +65,7 @@ class AuthRepositoryWithApi @Inject constructor(
         password: String,
         user: User
     ): Result<AuthModel, Error> {
-        return if (!appContext.isNetworkAvailable()) {
+        return if (appContext.isNetworkAvailable()) {
             try {
                 val signUpDto = SignUpDto(
                     userName,
@@ -66,9 +78,8 @@ class AuthRepositoryWithApi @Inject constructor(
                     400 -> {
                         val json = e.response()?.errorBody()?.string()
                         val type = object : TypeToken<CompoundError<SignUpError>>() {}.type
-                        gson.fromJson(json, type) ?: CompoundError(SignUpError.UNEXPECTED)
+                        gson.fromJson(json, type) ?: SignUpError.UNEXPECTED
                     }
-
                     401, 403 -> NetworkError.FORBIDDEN
                     404 -> NetworkError.NOT_FOUND
                     in 500..599 -> NetworkError.SERVER_ERROR
@@ -80,10 +91,11 @@ class AuthRepositoryWithApi @Inject constructor(
             } catch (_: IOException) {
                 Result.Failure(NetworkError.SERVER_NOT_AVAILABLE)
             } catch (_: Exception) {
-                Result.Failure(CompoundError(SignUpError.UNEXPECTED))
+                Result.Failure(SignUpError.UNEXPECTED)
             }
         } else Result.Failure(NetworkError.CONNECTION_ERROR)
     }
+
     override suspend fun signOut(userId: Long): Result<Unit, SignOutError> {
         return try {
             Result.Success(Unit)
