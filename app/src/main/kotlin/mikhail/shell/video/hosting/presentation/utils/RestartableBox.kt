fac26782fb20568da6bda37679915c458edd1f63
Mikhail_Shell <theoutstandingme@gmail.com>
2026-01-24 20:39:41 +0700

    Fixed RestartableBox

--- app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/RestartableBox.kt
+++ app/src/main/kotlin/mikhail/shell/video/hosting/presentation/utils/RestartableBox.kt
@@ -1,6 +1,7 @@
 package mikhail.shell.video.hosting.presentation.utils
 
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -15,11 +16,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.SaverScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,19 +40,24 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import mikhail.shell.video.hosting.ui.theme.disabled
 
-private val reloadIndicatorSize = 20.dp
+private const val duration = 400
+private val reloadIndicatorSize = 24.dp
 private val reloadThumbSize = 1.5 * reloadIndicatorSize
 private val shadowBaseDiameter = 1.2 * reloadThumbSize
 private val shadowWidth = 5.dp
 private val topPosition = -(shadowBaseDiameter + shadowWidth)
 private val bottomPosition = 0.7f * (shadowBaseDiameter + shadowWidth)
+private val activationZone = bottomPosition..(1.2f * (bottomPosition - topPosition))
 
 private val dpSaver = object : Saver<MutableState<Dp>, Float> {
     override fun SaverScope.save(value: MutableState<Dp>): Float {
         return value.value.value
     }
-
     override fun restore(value: Float): MutableState<Dp> {
         return mutableStateOf(value.dp)
     }
@@ -63,50 +71,58 @@ fun RestartableBox(
     onStart: () -> Unit,
     content: @Composable () -> Unit
 ) {
-    val TAG = "RestartableBox"
     val resistance = 0.15f
     val density = LocalDensity.current.density
     var height by rememberSaveable(saver = dpSaver) { mutableStateOf(topPosition) }
-    val animatedHeight by animateDpAsState(height, tween(200))
-    var isPressed by rememberSaveable { mutableStateOf(false) }
-    var isDragged by rememberSaveable { mutableStateOf(false) }
+    val animatedHeight = remember {
+        Animatable(
+            initialValue = height,
+            typeConverter = Dp.VectorConverter
+        )
+    }
+    val updatedHeight by rememberUpdatedState(height)
     val canStartUpdated by rememberUpdatedState(canStart)
-    val isStartingUpdated by rememberUpdatedState(isStarting)
+    var isStartingCurrent by rememberSaveable {
+        mutableStateOf(isStarting)
+    }
+    val isStartingUpdated by rememberUpdatedState(isStartingCurrent)
     Box(
         modifier = modifier
             .clipToBounds()
             .pointerInput(Unit) {
-                awaitPointerEventScope {
-                    while (true) {
-                        val event = awaitPointerEvent(pass = PointerEventPass.Initial)
-                        event.changes.firstOrNull()?.let { change ->
+                coroutineScope {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent(pass = PointerEventPass.Initial)
+                            val change = event.changes.firstOrNull() ?: continue
                             when {
                                 !change.previousPressed && change.pressed -> { // Pointer down
-                                    isPressed = true
-                                    isDragged = false
                                 }
+
                                 change.pressed && change.positionChange() != Offset.Zero -> { // Dragging
-                                    isDragged = true
                                     if (!isStartingUpdated && canStartUpdated) {
                                         height =
-                                            (height + (change.positionChange().y * density * resistance).dp).coerceIn(
+                                            (updatedHeight + (change.positionChange().y * density * resistance).dp).coerceIn(
                                                 topPosition,
-                                                bottomPosition
+                                                activationZone.endInclusive
                                             )
-                                        if (!(height == topPosition && change.positionChange().y < 0)) {
+                                        if (!(updatedHeight == topPosition && change.positionChange().y < 0)) {
                                             change.consume()
                                         }
                                     }
                                 }
+
                                 change.previousPressed && !change.pressed -> { // Pointer up or Cancelled
-                                    isDragged = false
-                                    isPressed = false
-                                    if (height == bottomPosition) {
-                                        if (canStartUpdated) {
-                                            onStart()
+                                    launch {
+                                        animatedHeight.snapTo(updatedHeight)
+                                        if (updatedHeight in activationZone) {
+                                            if (canStartUpdated) {
+                                                animatedHeight.animateTo(bottomPosition, tween(duration))
+                                                onStart()
+                                            }
+                                        } else {
+                                            animatedHeight.animateTo(topPosition, tween(duration))
                                         }
-                                    } else if (height < bottomPosition) {
-                                        height = topPosition
                                     }
                                 }
                             }
@@ -118,18 +134,22 @@ fun RestartableBox(
     ) {
         content()
         RestartThumb(
-            modifier = Modifier.offset(
-                y = when {
-                    isDragged || isPressed || isStartingUpdated -> height
-                    else -> animatedHeight
-                }
-            ),
-            isStarting = isStartingUpdated
+            modifier = Modifier.offset(y = height),
+            isActive = height in activationZone,
+            isStarting = isStarting
         )
+    }
+    LaunchedEffect(Unit) {
+        snapshotFlow { animatedHeight.value }.collect {
+            height = it
+        }
     }
     LaunchedEffect(isStarting) {
         if (!isStarting) {
-            height = topPosition
+            delay(duration.toLong())
+            isStartingCurrent = isStarting
+            animatedHeight.snapTo(height)
+            animatedHeight.animateTo(topPosition, tween(duration))
         }
     }
 }
@@ -137,8 +157,18 @@ fun RestartableBox(
 @Composable
 private fun RestartThumb(
     modifier: Modifier = Modifier,
+    isActive: Boolean,
     isStarting: Boolean
 ) {
+    var isStartingCurrent by rememberSaveable {
+        mutableStateOf(isStarting)
+    }
+    LaunchedEffect(isStarting) {
+        if (!isStarting) {
+            delay(duration.toLong())
+        }
+        isStartingCurrent = isStarting
+    }
     var angle by rememberSaveable {
         mutableFloatStateOf(0f)
     }
@@ -162,10 +192,13 @@ private fun RestartThumb(
             },
         contentAlignment = Alignment.Center
     ) {
-        if (isStarting) {
+        if (isStartingCurrent) {
             CircularProgressIndicator(
                 modifier = Modifier.size(reloadIndicatorSize),
-                color = MaterialTheme.colorScheme.primary
+                color = when (isActive) {
+                    true -> MaterialTheme.colorScheme.primary
+                    false -> MaterialTheme.colorScheme.disabled
+                }
             )
         } else {
             CircularProgressIndicator(
